e8a620936424f43f1e1d17c8d541afabd1cb3fc8
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-07-20 17:34:14 -0400

    Api class

---
+++ warehouse-app-frontend/services/api.js
@@ -0,0 +1,11 @@
+class API {
+    // render all warehouses index page
+    static loadWarehouses() {
+      fetch(`http://localhost:3000/warehouses`)
+        .then(res => res.json())
+        .then(warehouseData => {
+          for(let whs of warehouseData) {
+            const {name, capacity, whs_items, id} = warehouse;
+            new Warehouse(name, capacity, whs_items, id);
+        }})
+    };
