543ec985ee62246b7ad69c75d0605a449e9f3b5f
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-07-21 00:32:11 -0400

    whs_item script constructors

---
+++ warehouse-app-frontend/models/whs_item.js
@@ -0,0 +1,9 @@
+class WhsItem {
+    constructor(name, description, price, quantity) {
+      this.name = name;
+      this.description = description;
+      this.price = price;
+      this.quantity = quantity;
+    };
+
+};
